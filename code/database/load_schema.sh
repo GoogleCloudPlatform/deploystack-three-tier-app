15206d08aa13dbb3fd3de9cb58e11e0a2bba7f10
Terrence Ryan <tpryan@google.com>
2022-06-10 16:08:37 -0700

    Whoa, how long was this broken?

--- code/database/load_schema.sh
+++ code/database/load_schema.sh
@@ -1,7 +1,7 @@
 PROJECT=$1
 SQLNAME=$2
 
-SQLSERVICEACCOUNT=$(gcloud sql instances describe $SQLNAME --project$PROJECT --format="value(serviceAccountEmailAddress)" | xargs)
+SQLSERVICEACCOUNT=$(gcloud sql instances describe $SQLNAME --project $PROJECT --format="value(serviceAccountEmailAddress)" | xargs)
 gsutil mb gs://$PROJECT-temp 
 gsutil cp schema.sql gs://$PROJECT-temp/schema.sql
 gsutil iam ch serviceAccount:$SQLSERVICEACCOUNT:objectViewer gs://$PROJECT-temp/

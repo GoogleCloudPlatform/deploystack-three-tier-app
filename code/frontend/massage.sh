5d378d0edf5f0d99b0067f5d170ccda87e53b0de
Terrence Ryan <tpryan@google.com>
2022-08-31 00:56:59 -0700

    fix: updated fe massage

--- code/frontend/massage.sh
+++ code/frontend/massage.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-API=$(gcloud run services describe todo-api --region=$1 --format="value(status.url)")
+API=$(gcloud run services describe three-tier-app-api --region=$1 --format="value(status.url)")
 stripped=$(echo ${API/https:\/\//})
 sed -i"" -e "s/127.0.0.1:9000/$stripped/" www/js/main.js
 
